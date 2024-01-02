04b6576d19792d3474d43e268e7d100406a858b4
tusharbansal22 <tusharbansal@iitbhilai.ac.in>
2024-01-02 16:48:32 +0530

    add : models

---
+++ models/orderModel.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const OrderItem = require('../models/orderItem');
+
+const orderSchema = mongoose.Schema({
+  _id : mongoose.Schema.Types.ObjectId,
+  restaurant_id:{
+    type:mongoose.Schema.Types.ObjectId,
+    required:true,
+  },
+  orderItems:  [
+    {
+      type : OrderItem
+    }
+  ],
+  payment_status : {
+    type: Number,
+    required:true,
+  }
+
+})
+
+module.exports = mongoose.model('Order', orderSchema);
