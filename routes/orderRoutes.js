14a63f4e66e53b6a03f0c4d87c4ff735c32893a8
Aditya Sankhla <adityasa@iitbhilai.ac.in>
2024-01-04 19:23:25 +0530

    created the flow and the routes

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -1,10 +1,15 @@
 const express = require("express");
-const orderController = require("../controllers/orderController");
+const vendorOrderController = require("../controllers/vendorOrderController");
+const userOrderController = require("../controllers/userOrderController");
 
 const router = express.Router();
 
-router.post("/", orderController.createOrder);
+router.post("/user", userOrderController.createOrder);
 
-router.get("/", orderController.getOrders);
+router.get("/user", userOrderController.getOrderHistory); //Add a query as a parameter to get the latest order status in the frontend.
+
+router.post("/vendor", vendorOrderController.orderStatus);
+
+router.get("/vendor", vendorOrderController.getOrders);
 
 module.exports = router;
