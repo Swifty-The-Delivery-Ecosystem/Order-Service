9e95a333094faea9df2fae282b35c99387d0b928
krishnan05 <aayushkr@iitbhilai.ac.in>
2024-01-26 12:14:07 +0530

    Added middlewares

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -12,17 +12,18 @@ const {
 } = require("../controllers/vendorOrderControl");
 
 const router = express.Router();
-
+const checkAuth = require("../middlewares/checkAuth");
+const checkVendor = require("../middlewares/checkVendor");
 // Add middleware to all the apis below.
 
-router.post("/user", createOrder);
+router.post("/user",checkAuth, createOrder);
 
-router.get("/users/:user_id/:active?",getOrderHistory); //Add a query as a parameter to get the latest order status in the frontend.
+router.get("/users/:user_id/:active?",checkAuth, getOrderHistory); //Add a query as a parameter to get the latest order status in the frontend.
 
-router.put("/vendor/order_update", updateOrderStatus);
+router.put("/vendor/order_update", checkVendor, updateOrderStatus);
 
 // router.get("/vendor/:vendor_id", getOrders);
 
-router.put("/orderstatus",updateConfirmedOrderStatus);
+router.put("/orderstatus", updateConfirmedOrderStatus);
 
 module.exports = router;
